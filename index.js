4abd0c5afdae7db08dd447343f770283c181c255
Donna Chen <donnaxtc@gmail.com>
2026-03-19 13:47:49 -0400

    changed variables

---
+++ index.js
@@ -0,0 +1,20 @@
+let code1 = 7 + 3;
+console.log(code1);
+
+let code2 = 60 / 2 + 10
+console.log(code2);
+
+let code3 = 39
+console.log(code3);
+
+let message = "The vault has been secured. The code to the vault is "
+console.log(message);
+
+let codeA = (code1 + "-" + code2 + "-" + code3);
+console.log(codeA);
+
+let codeB = (`${code1}-${code2}-${code3}`);
+console.log(codeB);
+
+console.log(message,codeA,codeB);
+
